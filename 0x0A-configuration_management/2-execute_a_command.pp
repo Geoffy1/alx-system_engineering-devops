37f29cedf947593b6eeaf4d655a41aa4c5312fc4
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-01-11 22:27:26 +0300

    Create 2-execute_a_command.pp

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# kill a process named killmenow
+
+exec {'kill process killmenow':
+    command  => 'pkill killmenow',
+    path     => '/usr/bin',
+    onlyif   => 'pgrep killmenow',
+    provider => shell
+}
